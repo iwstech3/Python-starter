97c2ee9319b3226e74decb5df35978218cedcb5f
Fonyuy Gita <FonyuyJudegita@gmail.com>
2025-01-25 01:14:52 -0800

    added hangman art

---
+++ 5.1 functions_projects/5.1.2 hangman.py
@@ -0,0 +1,58 @@
+print('''
+      
+      88                                                                            
+88                                                                            
+88                                                                            
+88,dPPYba,  ,adPPYYba, 8b,dPPYba,   ,adPPYb,d8 88,dPYba,,adPYba,  ,adPPYYba,  
+88P'    "8a ""     `Y8 88P'   `"8a a8"    `Y88 88P'   "88"    "8a ""     `Y8  
+88       88 ,adPPPPP88 88       88 8b       88 88      88      88 ,adPPPPP88  
+88       88 88,    ,88 88       88 "8a,   ,d88 88      88      88 88,    ,88  
+88       88 `"8bbdP"Y8 88       88  `"YbbdP"Y8 88      88      88 `"8bbdP"Y8  
+                                    aa,    ,88                                
+                                     "Y8bbdP"                                 
+             
+      
+      ''')
+
+
+print(''' 
+      
+       ___________.._______
+| .__________))______|
+| | / /      ||
+| |/ /       ||
+| | /        ||.-''.
+| |/         |/  _  \
+| |          ||  `/,|
+| |          (\\`_.'
+| |         .-`--'.
+| |        /Y . . Y\
+| |       // |   | \\
+| |      //  | . |  \\
+| |     ')   |   |   (`
+| |          ||'||
+| |          || ||
+| |          || ||
+| |          || ||
+| |         / | | \
+""""""""""|_`-' `-' |"""|
+|"|"""""""\ \       '"|"|
+| |        \ \        | |
+: :         \ \       : :  sk
+. .          `'       . .
+      
+      ''')
+
+# TASK 1: GUESSING A LETTER IN A CHOSEN WORD
+words = ('ant baboon badger bat bear beaver camel cat clam cobra cougar '
+         'coyote crow deer dog donkey duck eagle ferret fox frog goat '
+         'goose hawk lion lizard llama mole monkey moose mouse mule newt '
+         'otter owl panda parrot pigeon python rabbit ram rat raven '
+         'rhino salmon seal shark sheep skunk sloth snake spider '
+         'stork swan tiger toad trout turkey turtle weasel whale wolf '
+         'wombat zebra ').split()
+
+print(words)
+
+
+
